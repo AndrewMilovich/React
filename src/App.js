b460bf5f91f3acbb8c9a620446a0a8d9463e5a46
AndrewMilovich <triplle@ukr.net>
2022-01-16 19:06:47 +0200

    cw2 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import Form from "./components/Form/Form";
+import Cars from "./components/Cars/Cars";
+import {useState} from "react";
+
 function App() {
+  const [trigger,setTrigger] =useState(null)
+    const update=data=>{setTrigger(data)}
     return (
         <div>
-
+            <Form update={update}/>
+            <Cars trigger={trigger}/>
         </div>
     );
 }
