a18c2def4ab716a4a764dff6f85d7a2d1a6a43f6
AndrewMilovich <triplle@ukr.net>
2022-01-24 16:28:11 +0200

    hw3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import User from "./components/User/User";
-import UserDetails from "./components/Userdetais/UserDetails";
-import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import {useState} from "react";
 
 function App() {
-
     return (
         <div>
-<div>
-    <Users/>
 
-</div>
-            <Posts/>
         </div>
     );
 }
--- src/service/axios.service.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import baseURL from "../config/urls";
-
-
-export const axiosService = axios.create({
-    baseURL
-});
